c168c081b33300eca87ee60838b3c0732f8d7960
sayyidhusein <zahrahusein751@gmail.com>
2025-06-20 20:06:20 +0700

    terbaru

--- src/pages/api/kursus.ts
+++ src/pages/api/kursus.ts
@@ -4,7 +4,10 @@ import clientPromise from '../../lib/mongodb'
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {
+) { 
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
   const client = await clientPromise;
   const db = client.db("elearning");
 
